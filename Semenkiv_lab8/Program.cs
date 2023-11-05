2d32489a047dad1296427308566f1c3c83de094d
semenkivv <101628506+semenkivv@users.noreply.github.com>
2023-11-05 16:42:10 +0200

    Add files via upload

---
+++ Semenkiv_lab8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba_7_V_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task("do work");
+            Task task2 = new Task("do sleap");
+            Task task3 = new Task("do laba 3");
+            Task task4 = new Task("meet with manager");
+            Task task5 = new Task("do relax");
+            ListTasks listTasks = new ListTasks(task4, task3, task5);
+            listTasks.AddElement(task1);
+            listTasks.AddElement(task2);
+
+            // Json 
+
+            FileManagerListTasks.SerializationToJSON(listTasks.tasks, "ListTasks.json");
+
+            List<Task> listTasksJson = FileManagerListTasks.DeserializationFromJSON("ListTasks.json");
+            Console.WriteLine($"From (ListTasks.json)  :");
+            foreach (Task task in listTasksJson)
+            {
+                Console.WriteLine(task);
+                Console.WriteLine("------------------------");
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+
+            // Bin 
+            
+            FileManagerListTasks.SerializationToBinary(listTasks.tasks, "ListTasks.bin");
+
+            List<Task> listTasksBinary = FileManagerListTasks.DeserializationFromBinary("ListTasks.bin");
+            Console.WriteLine($"From (ListTasks.bin)  :");
+            foreach (Task task in listTasksBinary)
+            {
+                Console.WriteLine(task);
+                Console.WriteLine("------------------------");
+            }
+        }
+    }
+}
---
+++ Semenkiv_lab8/Task.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Laba_7_V_11
+{
+    [Serializable]
+    public class Task
+    {
+        private bool _isCompleted;
+        private string _description;
+        private DateTime _dateCreated;
+        private DateTime _dateCompleated;
+
+        public Task(string description)
+        {
+            IsCompleted = false;
+            Description = description;
+            DateCreated = DateTime.Now;
+        }
+
+        [JsonPropertyName("IsCompleted")]
+        /// <summary>
+        /// Свойство выполнена ли задача
+        /// </summary>
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                if (value)
+                {
+                    DateCompleated = DateTime.Now;
+                }
+                _isCompleted = value;
+            }
+        }
+
+        [JsonPropertyName("Description")]
+        /// <summary>
+        /// Свойство описание задачи , возвращает ArgumentNullException, если пытаемся присвоить пустую строку
+        /// </summary>
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _description = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
+
+        [JsonPropertyName("DateCreated")]
+        /// <summary>
+        ///  Свойство дата создания задачи, возвращает ArgumentException если некоректная дата
+        /// </summary>
+        public DateTime DateCreated
+        {
+            get => _dateCreated;
+            init
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _dateCreated = value;
+                }
+            }
+        }
+
+        [JsonPropertyName("DateCompleated")]
+        /// <summary>
+        ///  Свойство дата, когда выполненили задачу, возвращает ArgumentException если некоректная дата
+        /// </summary>
+        public DateTime DateCompleated
+        {
+            get => _dateCompleated;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _dateCompleated = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            string isCompleted = (IsCompleted ? "" : "not ")+ "compleated" + (IsCompleted ? " at ="+DateCompleated : "") ;
+            return $"Task ({isCompleted})\nDescription={Description};  \nCreated  ={DateCreated}; \n";
+        }
+    }
+}
